cbe7c9a384a69aa504805f99981f01dacebe7b68
varund31 <varunrajaya@gmail.com>
2020-03-05 15:36:48 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,5 @@
-print("Hello World")
+from datetime import date
+from nsepy import get_history
+sbin = get_history(symbol='SBIN',
+                   start=date(2020,1,1),
+                   end=date(2020,1,10))
